3e73eac1d18ce9b30fafe6c3b1a0f65d30a9f944
Marjorie <marjorieynumarivera@gmail.com>
2023-03-03 21:06:09 -0500

    foto vitates

--- resources/views/deseos.blade.php
+++ resources/views/deseos.blade.php
@@ -14,51 +14,45 @@
                         <h2>Nuestros padres</h2>
                     </div>
                 </div>
-                <div class="row">
-                    {{--<div class="">  col-md-12 text-center animate-box --}}
-                        {{-- <div class=""> --}}
-                            <div class="couple-wrap text-center animate-box fadeInUp animated-fast">
-                                <div class="couple-half col-md-4 col-sm-6">
-                                    <div class="thumbnail" style="border: none;">
-                                        <img src="{{ config('app.url') }}/images/D_PV.jpeg" alt="..." style="max-width: 50%;
-                                        ">
-                                        <div class="testimony-slide" style="padding: 9px; padding-top: 4rem;">
-                                            <h3 class="text-center">David y Patricia Vitate</h3>
-                                            <p>
-                                                Queridos hijos : Efectivamente la luz atrae a luz, eso es lo que ustedes
-                                                son. Es mi eterna oración que juntos trabajen por que esa luz brille por
-                                                siempre, que cada uno ponga su cuota de perdón, valor y sacrificio. Sé
-                                                que el Padre Celestial les reserva un lugar a su lado. No estan solos
-                                                para lograrlo, solo recuerden y vivan todo lo que hasta ahora ya saben y
-                                                habran sus mentes y sus corazones con humildad y amor para lo que aún
-                                                necesitan aprender. Qué ni la prisa de esta vida jamás les haga olvidar
-                                                que Cristo nuestro Salvador esta en medio de toda desición. Una vez mas
-                                                sera mi oración de cada día que el Señor ponga sus manos en día de
-                                                vidas.<br><br>
-                                                Les ama Papá David y Mamá Patty
-                                            </p>
-                                            {{-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a
+                <div class="couple-wrap text-center animate-box fadeInUp animated-fast">
+                    <div class="couple-half col-md-4 col-sm-6">
+                        <div class="thumbnail" style="border: none;">
+                            <img src="{{ config('app.url') }}/images/D_1_PD.jpg" alt="..." style="max-width: 50%;">
+                            <div class="testimony-slide" style="padding: 9px; padding-top: 4rem;">
+                                <h3 class="text-center">David y Patricia Vitate</h3>
+                                <p>
+                                    Queridos hijos : Efectivamente la luz atrae a luz, eso es lo que ustedes
+                                    son. Es mi eterna oración que juntos trabajen por que esa luz brille por
+                                    siempre, que cada uno ponga su cuota de perdón, valor y sacrificio. Sé
+                                    que el Padre Celestial les reserva un lugar a su lado. No estan solos
+                                    para lograrlo, solo recuerden y vivan todo lo que hasta ahora ya saben y
+                                    habran sus mentes y sus corazones con humildad y amor para lo que aún
+                                    necesitan aprender. Qué ni la prisa de esta vida jamás les haga olvidar
+                                    que Cristo, nuestro Salvador, esta en medio de toda desición. Una vez más
+                                    será mi oración de cada día que el Señor ponga sus manos en día de
+                                    vidas.<br><br>
+                                    Les ama Papá David y Mamá Patty
+                                </p>
+                                {{-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a
                                                     href="#" class="btn btn-default" role="button">Button</a></p> --}}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="couple-half col-md-4 col-sm-6 ">
-                                    <div class="thumbnail" style="border: none;">
-                                        <img src="{{ config('app.url') }}/images/D_DY.jpeg" alt="..." style="max-width: 50%;
-                                        ">
-                                        <div class="testimony-slide" style="padding: 9px;padding-top: 4rem;">
-                                            <h3 class="text-center">Segundo y Dali Ynuma</h3>
-                                            <p>Muchas bendiciones en su nueva vida. Les deseo con mucho amor que sean
-                                                muy felices. Qué Dios los bendiga</p>
-                                            {{-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a
-                                                    href="#" class="btn btn-default" role="button">Button</a></p> --}}
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
-                        {{-- </div> --}}
-                    {{-- </div> --}}
-                </div>
+                        </div>
+                    </div>
+                    <div class="couple-half col-md-4 col-sm-6 ">
+                        <div class="thumbnail" style="border: none;">
+                            <img src="{{ config('app.url') }}/images/D_DY.jpeg" alt="..."
+                                style="max-width: 50%;
+                                        ">
+                            <div class="testimony-slide" style="padding: 9px;padding-top: 4rem;">
+                                <h3 class="text-center">Segundo y Dali Ynuma</h3>
+                                <p>Muchas bendiciones en su nueva vida. Les deseo con mucho amor que sean
+                                    muy felices. Qué Dios los bendiga</p>
+                                {{-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a
+                                                    href="#" class="btn btn-default" role="button">Button</a></p> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>                
             </div>
         </div>
     </div>
@@ -122,4 +116,6 @@
             </div>
         </div>
     </div>
+
+    
 </x-app-layout>
