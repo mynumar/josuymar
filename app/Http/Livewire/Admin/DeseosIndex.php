9098af91c370c0754afc14e0507b5837b4a04fa2
Josué <josue.vitate@gmail.com>
2023-04-19 19:52:34 -0500

    paignacion , readyto load

--- app/Http/Livewire/Admin/DeseosIndex.php
+++ app/Http/Livewire/Admin/DeseosIndex.php
@@ -41,7 +41,7 @@ class DeseosIndex extends Component
     {
         $deseos = [];
         if ($this->readyToLoad) {
-            $deseos = Deseo::paginate('5');
+            $deseos = Deseo::paginate('50');
         }
 
         $this->resetPage();
